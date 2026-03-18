0c07a8250bf91b5f4469d8bbddc32b59fe8c17fb
Sekouba CAMARA <sekoubacamara100@gmail.com>
2026-03-18 14:54:33 -0400

    connected my DB to my server

---
+++ db/connection.js
@@ -0,0 +1,19 @@
+// import the mongoose to help with the connection
+import {mongoose} from 'mongoose'
+import 'dotenv/config'
+
+// store the connection string to a variable
+const uri = process.env.MONGI_URI
+
+
+// make an async function to handle the connection and any errors
+async function conncetDB() {
+    try{
+        await mongoose.connect(uri)
+        console.log('MongoDB connected')
+    }catch(e){
+        console.error(e)
+    }
+}
+
+conncetDB()
